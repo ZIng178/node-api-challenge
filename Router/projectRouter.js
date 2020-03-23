ef877a7e41e0d019b42737bcd5e2433d5ed27705
Wangdi Tenzing <wangdi457@gmail.com>
2020-03-22 22:44:31 -0400

    POST AND PUT working

--- Router/projectRouter.js
+++ Router/projectRouter.js
@@ -25,7 +25,35 @@ router.get("/:id", (req,res)=>{
     })
 })
 
-router.get 
+router.post("/", (req,res)=>{
+    const body=req.body
+    projectdata.insert(body)
+    .then(projects=>{
+        res.status(201).json(projects)
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({message:"error posting"})
+    })
+})
 
+router.put("/:id", (req,res)=>{
+    const {id}=req.params
+    const changes=req.body
+    projectdata.update(id,changes)
+    .then(updated =>{
+        if(updated){
+            res.status(200).json(updated)
+        }else{
+            res.status(404).json({message:`project with the specific id does not exist`})
+        }
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({message :`error updating data`})
+    })
+        
+    
+})
 
 module.exports=router
