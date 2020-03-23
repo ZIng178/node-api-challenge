aa1493ea0ab25bee9922ff41d7116e2d46195bbc
Wangdi Tenzing <wangdi457@gmail.com>
2020-03-22 21:53:00 -0400

    DELETE is working for action router

--- Router/actionRouter.js
+++ Router/actionRouter.js
@@ -13,6 +13,17 @@ router.get("/", (req,res) =>{
     })
 })
 
+router.get("/:id", (req,res) =>{
+    actiondata.get()
+    .then(actions=>{
+        res.status(200).json(actions)
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({message: 'Error getting actions '})
+    })
+})
+
 router.post("/", (req,res)=>{
     const body=req.body
     actiondata.insert(body)
@@ -23,6 +34,22 @@ router.post("/", (req,res)=>{
         console.log(error)
         res.status(500).json({message :"Error posting actions"})
     })
+})
+
+router.delete("/:id", (req,res)=>{
+const {id}=req.params;
+actiondata.remove(id)
+.then(deleted=>{
+    if(deleted){
+        res.status(200).json({message: `action id ${id} was deleted`})
+    } else{
+        res.status(400).json({message : `action id ${id} not found `})
+    }
+})
+.catch(error=>{
+    console.log(error)
+    res.status(500).json({message:` cannot be deleted`})
+})
 })
 
 module.exports=router
